91b69137aa3a2314a6bb9c2d742e802d9e906914
khalid <khaledesam384@gmail.com>
2025-08-25 17:01:22 +0300

    feat: About us page creatd

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import "./App.css";
 import { VStack } from "@chakra-ui/react";
 import { Header } from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { Services } from "./pages/Services";
+import { Services } from "./pages/Services";
 // import { Blogs } from "./pages/Blogs";
 // import { Articles } from "./pages/Articles";
-// import { Contact } from "./pages/Contact";
-// import { About } from "./pages/About";
+import { Contact } from "./pages/ContactUs.tsx";
+import { About } from "./pages/AbostUs.tsx";
 import { Home } from "./pages/Home";
 import { Footer } from "./components/Footer";
-// import { NotFound } from "./pages/NotFound";
+import { NotFound } from "./pages/NotFound";
 import { useLanguage } from './hooks/useLanguage.ts';
 import { useEffect } from 'react';
 // import { ViewAllArticles } from "./components/ViewAllArticles";
@@ -37,29 +37,27 @@ function App() {
   useFontFamily(); // dynamically switches fonts
 
   return (
-    <VStack minH="100vh" >
+    <VStack minH="100vh" gap={0}>
       <BrowserRouter>
         <Header />
         <main>
           {/* <TimedPopup /> */}
           <Routes>
             <Route path="/" element={<Home />} />
-
-
-            {/* <Route path="/about" element={<About />} />
+            <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
-            <Route path="/knowledge" element={<KnowledgeCenter />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/blog/:slug" element={<BlogDetails />} />
-            <Route path="/blogs/all-blogs" element={<ViewAllBlogs />} />
-            <Route path="/articles" element={<Articles />} />
-            <Route path="/articles/all-articles" element={<ViewAllArticles />} />
-            <Route path="/article/:id" element={<ArticleDetails />} />
+            {/* <Route path="/knowledge" element={<KnowledgeCenter />} /> */}
+            {/* <Route path="/blogs" element={<Blogs />} /> */}
+            {/* <Route path="/blog/:slug" element={<BlogDetails />} /> */}
+            {/* <Route path="/blogs/all-blogs" element={<ViewAllBlogs />} /> */}
+            {/* <Route path="/articles" element={<Articles />} /> */}
+            {/* <Route path="/articles/all-articles" element={<ViewAllArticles />} /> */}
+            {/* <Route path="/article/:id" element={<ArticleDetails />} /> */}
             <Route path="/contact" element={<Contact />} />
-            <Route path="/terms-and-conditions" element={<TermsandConditions />} />
-            <Route path="/faq" element={<FrequentQuestions />} /> */}
+            {/* <Route path="/terms-and-conditions" element={<TermsandConditions />} /> */}
+            {/* <Route path="/faq" element={<FrequentQuestions />} />  */}
             {/* <Route path="/test" element={<TestComp/>} /> */}
-            {/* <Route path="*" element={<NotFound />} /> */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Toaster />
         </main>
---
+++ src/pages/AbostUs.tsx
@@ -0,0 +1,15 @@
+import { ContactBanner } from "@/components/contact_components/ContactBanner";
+
+export const About = () => {
+  return (
+    <>
+      <ContactBanner
+        bgPos="0% /27%"
+        img="About.jpg"
+        title="تواصل معنا"
+        subtitle=""
+        description=""
+      />
+    </>
+  );
+};
